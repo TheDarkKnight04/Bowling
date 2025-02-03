89e5995f6257f6b536690be46d02e7450c4f438f
TheDarkKnight04 <santam040802@gmail.com>
2025-02-02 22:36:16 -0800

    Added Reset Functionality

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+    [SerializeField] private BallController ball;
+    [SerializeField] private GameObject pinCollection;
+    [SerializeField] private Transform pinAnchor;
+    [SerializeField] private InputManager inputmanager;
     [SerializeField] private TextMeshProUGUI scoreText;
-    private FallTrigger[] pins;
+    private FallTrigger[] fallTriggers;
+    private GameObject pinObjects;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        inputmanager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
+    }
 
-        foreach (FallTrigger pin in pins)
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+
+    private void SetPins()
+    {
+        if(pinObjects)
+        {
+            foreach (Transform child in pinObjects.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Destroy(pinObjects);
+        }
+
+        pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.identity, transform);
+
+        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach (FallTrigger pin in fallTriggers)
         {
             pin.OnPinFall.AddListener(IncrementScore);
         }
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,6 +5,7 @@ public class InputManager : MonoBehaviour
 {
     public UnityEvent OnSpacePressed = new UnityEvent();
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnResetPressed = new UnityEvent();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +19,7 @@ public class InputManager : MonoBehaviour
         {
             OnSpacePressed?.Invoke();
         }
+        
         Vector2 input = Vector2.zero;
         if (Input.GetKey(KeyCode.A))
         {
@@ -28,5 +30,10 @@ public class InputManager : MonoBehaviour
             input += Vector2.right;
         }
         OnMove?.Invoke(input);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnResetPressed?.Invoke();
+        }
     }
 }
